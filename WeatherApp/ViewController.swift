68a4243c5a20a573ee8db18bfa540b54558582bd
ymagin <magin.yaroslav@gmail.com>
2019-08-02 11:39:25 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by  Магин Ярослав on 02/08/2019.
+//  Copyright © 2019 Magin Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
